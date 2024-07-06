93540e24ca1988ef3acd366ccbe524534e6f0661
Myung Hoon So <joa214@hotmail.com>
2024-07-07 05:09:15 +0900

    Added files

---
+++ WeatherApp/WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppApp.swift
+//  WeatherApp
+//
+//  Created by myung hoon on 07/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
